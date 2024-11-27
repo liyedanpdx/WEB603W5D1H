eab0a4258b0d6fd68a30554f98e0e6d8b76b632a
Yedan Li <63386101+liyedanpdx@users.noreply.github.com>
2024-11-27 00:21:13 -0600

    Complete

---
+++ node-mongo/app/models/inventory.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const InventorySchema = new mongoose.Schema({
+    prodname: String,
+    qty: Number,
+    price: Number,
+    status: String
+});
+
+module.exports = mongoose.model('Inventory', InventorySchema);
+
+
